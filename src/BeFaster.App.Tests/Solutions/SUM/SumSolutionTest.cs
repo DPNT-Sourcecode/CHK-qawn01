56c6b27661ca9993dda904c744ad019fe913d4f6
sbx_user1051 <sbx_user1051@169.254.170.237>
2020-11-02 11:58:25 +0000

    Mon Nov 02 11:58:25 UTC 2020

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -7,9 +7,15 @@ namespace BeFaster.App.Tests.Solutions.SUM
     public class SumSolutionTest
     {
         [TestCase(1, 1, ExpectedResult = 2)]
+        [TestCase(1, 2, ExpectedResult = 3)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 6, ExpectedResult = 6)]
+        [TestCase(6, 0, ExpectedResult = 6)]
+        [TestCase(17, -23, ExpectedResult = -6)]
         public int ComputeSum(int x, int y)
         {
             return SumSolution.Sum(x, y);
         }
     }
 }
+
