42e5e2ca4803331179631e70d5cb5e72828c1d79
sbx_user1051 <sbx_user1051@169.254.13.69>
2020-11-02 13:33:46 +0000

    Mon Nov 02 13:33:46 UTC 2020

--- src/BeFaster.App/Solutions/CHK/DiscountConfig.cs
+++ src/BeFaster.App/Solutions/CHK/DiscountConfig.cs
@@ -1,6 +1,35 @@
-﻿namespace BeFaster.App.Solutions.CHK
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeFaster.App.Solutions.CHK
 {
     internal class DiscountConfig
     {
+        internal DiscountConfig(int discount, params DiscountQualifyingSku[] qualifyingSkus)
+        {
+            Discount = discount;
+            QualifyingSkus = qualifyingSkus;
+        }
+
+        internal DiscountQualifyingSku[] QualifyingSkus { get; }
+        internal int Discount { get;  }
+
+        internal int CalculateDiscount(Dictionary<char, int> skuCounts)
+        {
+            return Discount * QualifyingSkus.Min(qs => skuCounts[qs.Sku] / qs.QualifyingCount);
+        }
+    }
+
+    internal class DiscountQualifyingSku
+    {
+        internal DiscountQualifyingSku(char sku, int qualifyingCount)
+        {
+            Sku = sku;
+            QualifyingCount = qualifyingCount;
+        }
+
+        internal char Sku { get; }
+        internal int QualifyingCount { get; }
     }
 }
